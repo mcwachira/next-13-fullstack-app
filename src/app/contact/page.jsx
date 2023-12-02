fb47ec8004b601526b2ea8d3a8312056c75e5748
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-02 06:34:47 +0300

    chore :finished creating the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-import Button from "@/components/Button/Button";
+import Button from "../../components/Button/Button";
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image src="/contact.png" fill={true} alt="" className={styles.img} />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,34 @@ const Footer = () => {
     <div className={styles.container}>
       <div>©2023 Lamamia. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="mcwachira Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="mcwachira" />
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="mcwachira" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="mcwachira" />
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="mcwachira Facebook Account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="mcwachira"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="mcwachira"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="mcwachira"
+        />
       </div>
     </div>
   );
