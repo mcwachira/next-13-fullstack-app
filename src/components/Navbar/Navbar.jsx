d632df30cdcbea6b204d66a44fc3c80807ea6c49
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-03 09:58:30 +0300

    chore:Updated the Navbar to show log out button if use is authenticated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,47 @@
-"use client ";
+"use client";
+
 import Link from "next/link";
 import React from "react";
-import DarkModeToggle from "../DarkModeToggle/DarkMode";
 import styles from "./navbar.module.css";
-// import DarkModeToggle from "../DarkModeToggle/DarkMode";
-const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
+import DarkModeToggle from "../DarkModeToggle/DarkMode";
+import { signOut, useSession } from "next-auth/react";
 
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
@@ -50,8 +54,11 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
-
-        <button className={styles.logout}>Logout </button>
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
