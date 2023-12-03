8efe6622d3edb9392dc50a5e4a98da4044d9a1e4
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-03 09:57:10 +0300

    chore:created the Login page and login endpoint

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import connectDb from "../../../../utils/db";
+import User from "../../../../models/User";
 const handler = NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -9,7 +12,42 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
+
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        //Check if the user exists.
+        await connectDb();
+
+        try {
+          const user = await User.findOne({
+            email: credentials.email,
+          });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
